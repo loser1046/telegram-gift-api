a8285a9a2a2df88087ff6afd69310f5e93198ace
Echo <loser9509@gmail.com>
2025-03-19 17:18:21 +0800

    返回gift 动画

--- route/app.php
+++ route/app.php
@@ -20,7 +20,7 @@ Route::group( function () {
     Route::get('/myGifts/<type?>', 'User/getUserGifts');
     // Route::get('/myGifts', 'User/getUserGifts');
     Route::get('gifts', 'Lottery/index');
-    Route::get('giftAnimation', 'User/getGiftAnimation');
+    Route::get('giftAnimation/:gift_tg_id', 'User/getGiftAnimation');
     Route::post('doLottery/:type_id', 'Lottery/doLottery');
 
     // Route::get('/gift/createInvoiceLink', 'Lottery/createInvoiceLink');
